f1738afdf080ad600368506a75433896b04b7dd0
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-12-02 12:34:02 -0300

    Add responsive HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -16,20 +16,20 @@ function ButtonLink({ href, children, className }: ButtonLinkProps) {
 
 const HomePage = () => {
   return (
-    <div className="bg-[#F7F8FA] flex justify-center">
-      <div className="flex flex-col mt-10 h-screen max-w-4xl px-0 py-4">
-        <div className="container mx-auto px-6 py-4 bg-[#195383] flex max-w-4xl text-white rounded-xl justify-end">
-          <p className="text-7xl">
+    <div className="flex justify-center bg-[#F7F8FA]">
+      <div className="flex flex-col mt-10 h-screen max-w-64 px-0 py-4 /* responsivo */ lg:max-w-4xl">
+        <div className="container mx-auto px-6 py-4 bg-[#195383] flex max-w-64 text-white rounded-xl justify-end /* responsivo */ lg:max-w-4xl">
+          <p className="text-base lg:text-7xl">
             Bem-vindo ao projeto de
-            <p className="text-7xl text-end">dashboard escolar</p>
+            <p className="text-base text-end /* responsivo */ lg:text-7xl">dashboard escolar</p>
           </p>
         </div>
         <div className="flex justify-end">
-          <p className="text-gray-400 mt-5">
+          <p className="mt-5 text-gray-400 flex items-center text-sm /* responsivo */  lg:text-base">
             Projeto ainda em desenvolvimento
             <ButtonLink
               href="/dashboard/admin"
-              className="bg-[#195383] hover:opacity-90 py-2 px-4 rounded-lg text-white max-w-24 ml-5"
+              className="py-2 px-4 rounded-lg bg-[#195383] text-white max-w-24 ml-5 hover:opacity-90"
             >
               Acessar
             </ButtonLink>
